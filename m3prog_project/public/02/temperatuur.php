d502299f40486a6995696eef4279d0880b8d1acd
Asta <omaralinora.1@gmail.com>
2024-03-12 14:37:47 +0100

    tempre

--- m3prog_project/public/02/temperatuur.php
+++ m3prog_project/public/02/temperatuur.php
@@ -1,9 +1,11 @@
 <?php
+
 $fahrenheit = 74;
-$celsius = ($fahrenheit - 32) / 1.7;
-echo"{$fahrenheit} graden Fahrenheit = {$celsius} graden Celsius. <br/>";
+$celsius = ($fahrenheit - 32) / 1.8;
+echo "{$fahrenheit} graden Fahrenheit = {$celsius} graden Celsius. <br/>\n";
 
 $celsius = 76;
-$fahrenheit = 32; 
-echo "{$celsius} graden celsius = {$fahrenheit} graden Fahrenheit. <br/>";
+$fahrenheit = ($celsius * 1.8) + 32;
+echo "{$celsius} graden celsius = {$fahrenheit} graden Fahrenheit. <br/>\n";
+
 ?>
