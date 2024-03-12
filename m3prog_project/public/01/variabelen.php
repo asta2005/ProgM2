3ca16e44a5c8611de7ad77f10312a5fe163848ac
Asta <omaralinora.1@gmail.com>
2024-03-12 14:34:08 +0100

    01 af begin 02

---
+++ m3prog_project/public/01/variabelen.php
@@ -0,0 +1,16 @@
+<?php
+    $naam = "omar kahouach";
+    $woonplaats = "Amsterdam";
+    $leeftijd = 18;
+    $temperatuur = 74.5;
+
+    echo "Mijn naam is {$naam}, ik kom uit {$woonplaats} en ik ben {$leeftijd} jaar oud. Mijn huidige temperatuur is {$temperatuur} graden Celsius.";
+    ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Variabelen en Zin</title>
+</head>
+<body>
+</body>
+</html>
---
+++ m3prog_project/public/02/temperatuur.php
@@ -0,0 +1,10 @@
+<?php
+$fahrenheit = 74;
+$celsius = ($fahrenheit - 32) / 1.7;
+echo"{$fahrenheit} graden Fahrenheit = {$celsius} graden Celsius. <br/>";
+
+
+$celsius = 76;
+$fahrenheit = 32; 
+echo "{$celsius} graden celsius = {$fahrenheit} graden Fahrenheit. <br/>";
+?>
